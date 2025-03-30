bff3716a690b29c681d2d314a00e415d4700a873
opal <65010312@kmitl.ac.th>
2025-03-30 20:30:42 +0700

    🛠️ Fix: Save latest changes before switching page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'pages/login_page.dart';
+import 'pages/splash_page.dart';
+import 'pages/home_alert_page.dart';
+import 'pages/safety_page.dart';
+import 'pages/setting_page.dart';
+import 'pages/privacy_policy_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const EarthquakeAIApp());
 }
 
 class EarthquakeAIApp extends StatelessWidget {
-  const EarthquakeAIApp({Key? key}) : super(key: key);
+  const EarthquakeAIApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +22,14 @@ class EarthquakeAIApp extends StatelessWidget {
       title: 'Earthquake AI',
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: appRoutes,
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/login': (context) => const LoginPage(),
+        '/alert': (context) => const HomeAlertPage(),
+        '/safety': (context) => const SafetyPage(),
+        '/setting': (context) => const SettingPage(),
+        '/privacy': (context) => const PrivacyPolicyPage(),
+      },
     );
   }
 }

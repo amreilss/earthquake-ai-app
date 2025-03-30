1252c5e351babf8f7f1acbe8ee47ac3a9d5907f1
opal <65010312@kmitl.ac.th>
2025-03-30 20:48:32 +0700

    🛠️ Fix: Save latest changes before switching page

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("com.google.gms.google-services") // ✅ สำคัญมาก
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.test_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // ✅ ตั้งให้ตรงกับที่ geolocator และ geocoding ต้องการ
-
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,20 +20,16 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.test_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true // ✅ ป้องกันปัญหา method limit
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -43,3 +38,4 @@ android {
 flutter {
     source = "../.."
 }
+
